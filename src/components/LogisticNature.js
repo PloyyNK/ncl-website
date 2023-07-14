bea2dbd9d95cc43c73ca7cfdd17903ad336391c9
Nartnatta Krivichian <nartnattakrivichian@Nartnattas-MacBook-Pro-2.local>
2023-07-14 13:36:23 +0700

    business component

---
+++ src/components/LogisticNature.js
@@ -0,0 +1,11 @@
+export default function LogisticsNature() {
+    return (
+        <div className='mt-20'>
+            <div className='title-line'>
+                <h1 className='font-bold text-2xl ml-8'>Summary of the nature of logistics management services</h1>
+                <div className='line-text'></div>
+            </div>
+            <img className='w-4/6 mx-auto mt-8 mb-8' src='https://www.nclthailand.com/uploads/upfiles/images/%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%93%E0%B8%B0%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%81%E0%B8%AD%E0%B8%9A%E0%B8%98%E0%B8%B8%E0%B8%A3%E0%B8%81%E0%B8%B4%E0%B8%88%20EN.jpg' />
+        </div>
+    )
+}
--- src/pages/Business.js
+++ src/pages/Business.js
@@ -4,6 +4,7 @@ import '../styles/Business.css'
 import Transitions from '../components/Transition'
 import ServiceCard from '../components/ServiceCard';
 import Partners from '../components/Partners';
+import LogisticsNature from '../components/LogisticNature';
 
 export default function Business() {
 
@@ -36,13 +37,8 @@ export default function Business() {
                     <ServiceCard />
                 </div>
                 <p className='text-right'>*Click to see more information</p>
-                <div className='mt-20'>
-                    <div className='title-line'>
-                        <h1 className='font-bold text-2xl ml-8'>Summary of the nature of logistics management services</h1>
-                        <div className='line-text'></div>
-                    </div>
-                    <img className='w-4/6 mx-auto mt-8 mb-8' src='https://www.nclthailand.com/uploads/upfiles/images/%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%93%E0%B8%B0%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%81%E0%B8%AD%E0%B8%9A%E0%B8%98%E0%B8%B8%E0%B8%A3%E0%B8%81%E0%B8%B4%E0%B8%88%20EN.jpg' />
-                </div>
+                
+                <LogisticsNature />
                 <Partners />
                 <Footer />
             </Transitions>
